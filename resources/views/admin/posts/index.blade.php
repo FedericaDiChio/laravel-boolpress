6108668bec67875bdc59c744839c8d9b2879d2ca
FedericaDiChio <federica.dichio7@gmail.com>
2021-10-22 01:18:06 +0200

    bottoni rotte crud

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     <section class="container">
-        <h1>I miei post</h1>
+        <header class="d-flex justify-content-between align-items-center">
+            <h1>I miei post</h1>
+            <a href="{{ route('admin.posts.create')}}" class="btn btn-success">Crea nuovo post</a>
+        </header>
         <table class="table table-hover bg-white border">
             <thead>
               <tr>
-                {{-- <th scope="col">#</th> --}}
                 <th scope="col">Titolo</th>
                 <th scope="col">Scritto il</th>
                 <th scope="col"></th>
@@ -15,11 +17,17 @@
             <tbody>
                 @forelse ($posts as $post)
               <tr>
-                <th scope="row">1</th>
                 <td>{{ $post->title}}</td>
                 <td>{{ $post->created_at}}</td>
-                <td><a href="{{ route('admin.posts.show', $post->id)}}" class="btn btn-primary">Vai</a></td>
-             
+                <td class="d-flex justify-content-end align-items-center">
+                    <a href="{{ route('admin.posts.show', $post->id)}}" class="btn btn-primary">Vai</a>
+                    <a href="{{ route('admin.posts.edit', $post->id)}}" class="btn btn-warning mx-2">Modifica post</a>
+                    <form action={{route('admin.posts.destroy', $post->id)}} method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger">Cancella post</button>
+                    </form>
+                </td>
               @empty
               <tr>
                   <td colspan="3" class="text-center">Non ci sono post da visualizzare</td>
