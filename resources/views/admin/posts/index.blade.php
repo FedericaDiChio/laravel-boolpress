77454f3d318158d510984c06ca892ff43c82f49e
FedericaDiChio <federica.dichio7@gmail.com>
2021-10-21 17:19:53 +0200

    aggiunta rotta index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <section class="container">
+        <h1>I miei post</h1>
+        <table class="table table-hover bg-white border">
+            <thead>
+              <tr>
+                {{-- <th scope="col">#</th> --}}
+                <th scope="col">Titolo</th>
+                <th scope="col">Scritto il</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+              <tr>
+                <th scope="row">1</th>
+                <td>{{ $post->title}}</td>
+                <td>{{ $post->created_at}}</td>
+                <td><a href="{{ route('admin.posts.show', $post->id)}}" class="btn btn-primary">Vai</a></td>
+             
+              @empty
+              <tr>
+                  <td>Non ci sono post da visualizzare</td>
+              </tr>
+              @endforelse
+            </tbody>
+          </table>
+    </section>
 @endsection
