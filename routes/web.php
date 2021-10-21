2bfd693a28e6d59f8ec67856f9c9cfede78a80cb
FedericaDiChio <federica.dichio7@gmail.com>
2021-10-21 16:44:36 +0200

    modello e controller Post

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
 Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function()  {
- Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts','PostController');
 });
